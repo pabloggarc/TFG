62eaa0e842d2ecf9960d5beb4323bc97e1e6951d
Pablo García <78372183+pabloggarc@users.noreply.github.com>
2024-02-12 14:58:06 +0100

    Tipos de aprendizaje, modificación en ejemplo Sobel, citas

--- Código/Scripts/ejemplo_sobel.m
+++ Código/Scripts/ejemplo_sobel.m
@@ -6,9 +6,10 @@ sobel_y = [-1 -2 -1; 0 0 0; 1 2 1];
 derivada_x = convn(double(rgb2gray(imagen)), sobel_x, 'valid');
 derivada_y = convn(double(rgb2gray(imagen)), sobel_y, 'valid');
 norma_gradiente = sqrt(derivada_x.^2 + derivada_y.^2); 
+norma_gradiente = mat2gray(norma_gradiente); 
 
 montage({imagen, 
         uint8(derivada_x), 
         uint8(derivada_y), 
-        uint8(norma_gradiente)
+        norma_gradiente
     }); 
